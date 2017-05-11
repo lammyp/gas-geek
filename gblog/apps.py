eb88cc42e8463f1f8b27ae38c99bc34f38ed7706
lammyp <lambert@internode.on.net>
2017-05-11 23:49:38 +0930

    gblog created

---
+++ gblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GblogConfig(AppConfig):
+    name = 'gblog'
---
+++ gblog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Article(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    subject_heading = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    recent_update = models.DateTimeField(default=timezone.now)
+
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.

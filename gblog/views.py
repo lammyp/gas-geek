d0a7a8b38853b3e58788d5ad43537ff368471908
lammyp <lambert@internode.on.net>
2017-05-12 23:22:48 +0930

    Posts changed to Articles, variables into template

--- gblog/views.py
+++ gblog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def article_list(request):
-    return render(request, 'gblog/article_list.html', {})
+    articles=Article.objects.all()
+    return render(request, 'gblog/article_list.html', {'articles': articles})
 
